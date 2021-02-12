2f78a5fa3bf8fbd363707fad884ea292697016c4
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2021-02-12 17:42:25 +0300

    ~disable CIVB

--- benchmark/src/jmh/kotlin/kotlinx/benchmarks/json/CoerceInputValuesBenchmark.kt
+++ benchmark/src/jmh/kotlin/kotlinx/benchmarks/json/CoerceInputValuesBenchmark.kt
@@ -8,12 +8,12 @@ import kotlinx.serialization.json.*
 import org.openjdk.jmh.annotations.*
 import java.util.concurrent.*
 
-@Warmup(iterations = 5, time = 1)
-@Measurement(iterations = 5, time = 1)
-@BenchmarkMode(Mode.Throughput)
-@OutputTimeUnit(TimeUnit.MICROSECONDS)
-@State(Scope.Benchmark)
-@Fork(2)
+//@Warmup(iterations = 5, time = 1)
+//@Measurement(iterations = 5, time = 1)
+//@BenchmarkMode(Mode.Throughput)
+//@OutputTimeUnit(TimeUnit.MICROSECONDS)
+//@State(Scope.Benchmark)
+//@Fork(2)
 open class CoerceInputValuesBenchmark {
 
     // Specific benchmark to isolate effect on #1156. Remove after release of 1.0.1
